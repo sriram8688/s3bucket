6d4d52c724a7bae765f2168d80c5ee3b0f004be7
solletisriramreddy123@gmail.com <whatsapchat1@gmail.com>
2023-06-14 17:24:47 +0530

    1srt

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider  "aws" {
+region = "ap-south-1"
+access_key = "AKIA3ZH5CUBMB2F2F7XM"
+secret_key = "zBbfssp1LpEVS6zaMi3WwZEWsADYh2emWwQCJ/W4"
+}
+
+resource "aws_s3_bucket" "example"   {
+  bucket = "sriram"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
